6aa70dceb5af2d6e1f670996c3831a43290dcc5d
ayshmnmm <65579576+ayshmnmm@users.noreply.github.com>
2020-07-22 12:29:04 +0530

    Added recents and more data. removed top

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ app.secret_key = os.environ.get('SECKEY', 'insert random string') # random secre
 
 ## ------ To run locally set localrun to True and use your client id and secret ----------------
 localrun = False
-cid = os.environ.get('CLID', 'insert client id') # spotify client id
-secret = os.environ.get('SECR', 'insert client secret') # spotify client secret
+cid = os.environ.get('CLID', 'insert ') # spotify client id
+secret = os.environ.get('SECR', 'insert') # spotify client secret
 
 ## --- also ensure http://127.0.0.1:5000/callback is there in your app's redirect uris
 
@@ -37,7 +37,6 @@ def static_file(path):
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
 API_BASE = 'https://accounts.spotify.com'
 
 if localrun :
@@ -53,7 +52,6 @@ def api_callback():
     session.clear()
     code = request.args.get('code')
     #print(code)
-
     auth_token_url = f"{API_BASE}/api/token"
     res = requests.post(auth_token_url, data={
         "grant_type":"authorization_code",
@@ -62,29 +60,34 @@ def api_callback():
         "client_id":cid,
         "client_secret":secret
         })
-
     res_body = res.json()
     #print(res.json())
     session["toke"] = res_body.get("access_token")
+    return redirect("recents")
 
-    return redirect("spotificate")
-
-
-@app.route("/spotificate")
+@app.route("/recents")
 def go():
     try :
+        count = 0
+        stats = [0,0,0,0,0,0,0,0] # danceability energy mood speechiness Acousticness tempo key popularity
+        key = ['danceability','energy','valence','speechiness','acousticness','tempo','key','popularity']
         sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager,auth=session['toke'])
         response = []
-        x=0
-        for i in range(5) :
-            flag=0
-            results = sp.current_user_top_tracks(offset=x,limit=20,time_range='long_term')
-
-            for item in (results['items']):
-                track = item['name']
-                response.append(('-', '-', " – ", track))
-            x+=20
-        return render_template("page1.html", data=response,len=len(response),name=sp.current_user()['display_name'])
+        results = sp.current_user_recently_played(limit=50)
+        for item in (results['items']):
+            # song name , artist , image url , track uri
+            response.append((item['track']['name'],item['track']['artists'][0]['name'],item['track']['album']['images'][2]['url'],item['track']['uri']))
+            audio_features = sp.audio_features(tracks=item['track']['id'])[0]
+            for i in range(len(stats)-1):
+                stats[i] += audio_features[key[i]]
+            stats[-1] += item['track']['popularity']
+            count+=1
+        for i in range(len(stats)):
+            stats[i] = (stats[i]/count)*100
+        stats[-1] /= 100
+        stats[-2] /= 100
+        stats[-3] /= 100
+        return render_template("recents.html", data=response,len=len(response),name=sp.current_user()['display_name'],stats=stats)
     except :
         auth_url = f'{API_BASE}/authorize?client_id={cid}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPE}&show_dialog={SHOW_DIALOG}'
         return redirect(auth_url)
